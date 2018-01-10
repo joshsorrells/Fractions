6709d6b3f8d625facfb7dc9ef356417d37cbe418
Josh Sorrells <jsorrells99@outlook.com>
2018-01-10 08:54:17 -0600

    Initial sharing of project

---
+++ Fractions.java
@@ -0,0 +1,35 @@
+
+public class Fractions
+{
+
+    private int num, denom;
+
+    public Fractions(int num, int denom)
+    {
+        this.num = num;
+        this.denom = denom;  
+    }
+
+    public Fractions(int num)
+    {
+        this.num = num;
+        denom = 1;
+    }
+    
+    public Fractions()
+    {
+     num = 0;
+     denom = 1;   
+    }
+    
+    public Fractions(Fractions other)
+    {
+     num = other.num;
+     denom = other.denom; 
+    }
+    
+    public String toString()
+    {   
+        return num + " / " + denom;   
+    }    
+}
---
+++ Runner.java
@@ -0,0 +1,10 @@
+
+
+public class Runner
+{
+    public static void main(String wenner[])
+    {
+     Fractions myFraction = new Fractions(); 
+     System.out.println(myFraction);
+    } 
+}
