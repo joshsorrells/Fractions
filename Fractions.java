11ac002cffde9431d8b2a229523c57b809b80b66
Josh Sorrells <jsorrells99@outlook.com>
2018-01-12 09:17:56 -0600

    Created public and private methods (simplify and gcf)

--- Fractions.java
+++ Fractions.java
@@ -1,13 +1,20 @@
 
 public class Fractions
 {
-
     private int num, denom;
-
+    
     public Fractions(int num, int denom)
     {
+        if(denom == 0)
+        {
+         throw new IllegalArgumentException("Denom must be greater than 0");   
+        }
+        else
+        {
         this.num = num;
-        this.denom = denom;  
+        this.denom = denom; 
+        } 
+        simplify();
     }
 
     public Fractions(int num)
@@ -44,10 +51,36 @@ public class Fractions
     {
       return new Fractions(denom, num);
     }
+    
+    public Fractions divide(Fractions f)
+    {
+        return new Fractions(num*f.denom, denom*f.num);
+    }
+    
+    public static int gcf(int a, int b)
+    {
+    int min = Math.min(a, b);
+    for (int i = min;i > 0; i++)
+    {
+    if(a%i == 0 && b%i == 0) return i;    
+    }
+    return 1;
+    }
+    
+    private void simplify()
+    {
+       int factor = gcf(num, denom);
+       num/=factor;
+       denom/=factor;
+    }
+            
+    
+    
+    
     public String toString()
     {   
         return num + " / " + denom;   
-    } 
+    }
     
     
 }
--- Runner.java
+++ Runner.java
@@ -4,9 +4,20 @@ public class Runner
 {
     public static void main(String wenner[])
     {
-     Fractions myFraction = new Fractions(5,6); 
+     Fractions myFraction = new Fractions(18,12); 
      Fractions newObject = new Fractions(3,4);
      Fractions reciprocal = myFraction.recipFrac();
-     System.out.println(reciprocal);
-    } 
+     System.out.println(myFraction);
+     /*Fractions myFraction = null;
+     try
+     {
+      myFraction = new Fractions(5,0);   
+     }
+     catch(Exception e)
+     {
+     System.out.println("Fractions need a non-zero deonominator");    
+     }
+     System.out.println(myFraction);
+     */
+    }  
 }
